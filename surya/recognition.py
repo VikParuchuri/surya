2479472eee5c3f5b8f3810d95309422b754a8350
Vik Paruchuri <vik.paruchuri@gmail.com>
2024-05-17 14:43:33 -0700

    Undo generation changes

--- surya/recognition.py
+++ surya/recognition.py
@@ -47,56 +47,39 @@ def batch_recognition(images: List, languages: List[List[str]], model, processor
         batch_pixel_values = torch.tensor(np.array(batch_pixel_values), dtype=model.dtype).to(model.device)
         batch_decoder_input = torch.from_numpy(np.array(batch_decoder_input, dtype=np.int64)).to(model.device)
 
-        token_count = 0
-        encoder_outputs = None
-        batch_predictions = [[] for _ in range(len(batch_images))]
-        sequence_scores = None
-
-        attention_mask = torch.ones_like(batch_decoder_input, device=model.device)
-        past_key_values = None
-
         with torch.inference_mode():
-            while token_count < settings.RECOGNITION_MAX_TOKENS:
-                cache_position = torch.tensor([token_count+ 1], device=model.device)
+            return_dict = model.generate(
+                pixel_values=batch_pixel_values,
+                decoder_input_ids=batch_decoder_input,
+                decoder_langs=batch_langs,
+                eos_token_id=processor.tokenizer.eos_id,
+                max_new_tokens=settings.RECOGNITION_MAX_TOKENS,
+                output_scores=True,
+                return_dict_in_generate=True
+            )
+            generated_ids = return_dict["sequences"]
 
-                return_dict  = model(
-                    decoder_input_ids=batch_decoder_input,
-                    decoder_attention_mask=attention_mask,
-                    decoder_langs=batch_langs,
-                    pixel_values=batch_pixel_values,
-                    encoder_outputs=encoder_outputs,
-                    past_key_values=past_key_values,
-                    return_dict=True,
-                )
+            # Find confidence scores
+            scores = return_dict["scores"] # Scores is a tuple, one per new sequence position.  Each tuple element is bs x vocab_size
+            sequence_scores = torch.zeros(generated_ids.shape[0])
+            sequence_lens = torch.where(
+                generated_ids > processor.tokenizer.eos_id,
+                torch.ones_like(generated_ids),
+                torch.zeros_like(generated_ids)
+            ).sum(axis=-1).cpu()
+            prefix_len = generated_ids.shape[1] - len(scores) # Length of passed in tokens (bos, langs)
+            for token_idx, score in enumerate(scores):
+                probs = F.softmax(score, dim=-1)
+                max_probs = torch.max(probs, dim=-1).values
+                max_probs = torch.where(
+                    generated_ids[:, token_idx + prefix_len] <= processor.tokenizer.eos_id,
+                    torch.zeros_like(max_probs),
+                    max_probs
+                ).cpu()
+                sequence_scores += max_probs
+            sequence_scores /= sequence_lens
 
-                logits = return_dict["logits"]
-                preds = torch.argmax(logits[:, -1], dim=-1)
-                scores = torch.max(F.softmax(logits, dim=-1), dim=-1).values
-                done = preds == processor.tokenizer.eos_id
-
-                if sequence_scores is None:
-                    sequence_scores = scores
-                else:
-                    scores[done == 1] = 0
-                    sequence_scores = torch.cat([sequence_scores, scores], dim=1)
-
-                encoder_outputs = (return_dict["encoder_last_hidden_state"],)
-                past_key_values = return_dict["past_key_values"]
-
-                if done.all():
-                    break
-
-                attention_mask = torch.cat([attention_mask, ~done.unsqueeze(1)], dim=1)
-
-                for j, (pred, status) in enumerate(zip(preds.cpu(), done)):
-                    if not status:
-                        batch_predictions[j].append(int(pred))
-
-                batch_decoder_input = preds.unsqueeze(1)
-                token_count += 1
-
-        sequence_scores = torch.sum(sequence_scores, dim=-1) / torch.sum(sequence_scores != 0, dim=-1)
-        detected_text = processor.tokenizer.batch_decode(batch_predictions)
+        detected_text = processor.tokenizer.batch_decode(generated_ids)
         detected_text = [truncate_repetitions(dt) for dt in detected_text]
         # Postprocess to fix LaTeX output (add $$ signs, etc)
         detected_text = [fix_math(text) if math and contains_math(text) else text for text, math in zip(detected_text, has_math)]
