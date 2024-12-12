573b762b1775561a31020314cbccc898527aa02c
Vik Paruchuri <vik.paruchuri@gmail.com>
2024-12-12 11:56:27 -0500

    Start to pull in new table model

--- surya/model/table_rec/config.py
+++ surya/model/table_rec/config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Dict
 
 import torch
 from transformers import PretrainedConfig
@@ -7,14 +8,35 @@ from transformers.utils import ModelOutput
 from surya.settings import settings
 
 BOX_DIM = 1024
-SPECIAL_TOKENS = 7
-MAX_ROWS = 384
+SPECIAL_TOKENS = 5
+MAX_BOXES = 150
+
+MERGE_KEYS = {
+    "none": 0,
+    "merge_up": 1,
+    "merge_down": 2,
+    "merge_both": 3
+}
+
+ID_TO_CATEGORY = {
+    0: 'Blank',
+    1: 'Table-row',
+    2: 'Table-column',
+    3: 'Table-cell',
+    4: 'Table'
+}
+
+BOX_PROPERTIES = [
+    ("bbox", 6, "regression"),
+    ("category", len(ID_TO_CATEGORY), "classification"),
+    ("merges", len(MERGE_KEYS), "classification"),
+    ("colspan", 1, "regression")
+]
 
 
 @dataclass
 class TableRecModelOutput(ModelOutput):
-    bbox_logits: torch.Tensor
-    class_logits: torch.Tensor | None = None
+    box_property_logits: Dict[str, torch.Tensor]
     hidden_states: torch.Tensor | None = None
 
 
@@ -27,11 +49,9 @@ class SuryaTableRecConfig(PretrainedConfig):
 
         encoder_config = kwargs.pop("encoder")
         decoder_config = kwargs.pop("decoder")
-        text_enc_config = kwargs.pop("text_encoder")
 
         self.encoder = encoder_config
         self.decoder = decoder_config
-        self.text_encoder = text_enc_config
         self.is_encoder_decoder = True
 
         if isinstance(decoder_config, dict):
@@ -58,7 +78,7 @@ class DonutSwinTableRecConfig(PretrainedConfig):
         patch_size=4,
         num_channels=3,
         embed_dim=128,
-        depths=[2, 2, 14, 2],
+        depths=[2, 2, 12, 2],
         num_heads=[4, 8, 16, 32],
         num_kv_heads=[4, 8, 16, 32],
         window_size=8,
@@ -68,7 +88,7 @@ class DonutSwinTableRecConfig(PretrainedConfig):
         attention_probs_dropout_prob=0.0,
         drop_path_rate=0.1,
         hidden_act="gelu",
-        use_absolute_embeddings=True,
+        use_absolute_embeddings=False,
         initializer_range=0.02,
         layer_norm_eps=1e-5,
         encoder_length=1024,
@@ -107,9 +127,12 @@ class SuryaTableRecDecoderConfig(PretrainedConfig):
 
     def __init__(
         self,
-        num_hidden_layers=3,
-        vocab_size=settings.TABLE_REC_MAX_ROWS + SPECIAL_TOKENS,
+        num_hidden_layers=10,
+        vocab_size=BOX_DIM + 1,
+        bbox_size=BOX_DIM,
         hidden_size=512,
+        property_embed_size=64,
+        box_embed_size=512 - 64,
         intermediate_size=4 * 512,
         encoder_hidden_size=1024,
         num_attention_heads=8,
@@ -121,14 +144,16 @@ class SuryaTableRecDecoderConfig(PretrainedConfig):
         use_cache=True,
         pad_token_id=0,
         eos_token_id=1,
-        bos_token_id=2,
+        bos_token_id=1,
+        pause_token_id=2,
+        query_end_token_id=5,
         hidden_activation="gelu_pytorch_tanh",
         rope_theta=10000.0,
         block_types=("attention",),
-        cross_attn_layers=(0, 1, 2, 3),
-        encoder_cross_attn_layers=(0, 1, 2, 3),
-        self_attn_layers=(0, 1, 2, 3),
-        global_attn_layers=(0, 1, 2, 3),
+        cross_attn_layers=tuple(range(10)),
+        encoder_cross_attn_layers=tuple(range(10)),
+        self_attn_layers=tuple(range(10)),
+        global_attn_layers=tuple(range(10)),
         attention_dropout=0.0,
         num_key_value_heads=4,
         attention_bias=False,
@@ -137,10 +162,9 @@ class SuryaTableRecDecoderConfig(PretrainedConfig):
         tie_word_embeddings=False,
         aux_heads=0, # How many n-token-ahead heads to add
         causal=True,
-        max_classes=2 + SPECIAL_TOKENS,
-        max_width=1024 + SPECIAL_TOKENS,
-        max_height=1024 + SPECIAL_TOKENS,
-        out_box_size=1024,
+        layer_norm_eps=1e-5,
+        dropout=0.0,
+        special_token_count=SPECIAL_TOKENS,
         **kwargs,
     ):
         self.num_hidden_layers = num_hidden_layers
@@ -174,93 +198,16 @@ class SuryaTableRecDecoderConfig(PretrainedConfig):
         self.encoder_hidden_size=encoder_hidden_size
         self.causal = causal
         self.encoder_cross_attn_layers = encoder_cross_attn_layers
-        self.max_classes = max_classes
-        self.max_width = max_width
-        self.max_height = max_height
-        self.out_box_size = out_box_size
-
-        super().__init__(
-            pad_token_id=pad_token_id,
-            bos_token_id=bos_token_id,
-            eos_token_id=eos_token_id,
-            **kwargs,
-        )
-
-    @property
-    def layers_block_type(self):
-        return (self.block_types * 100)[: self.num_hidden_layers]
-
-
-class SuryaTableRecTextEncoderConfig(PretrainedConfig):
-    model_type = "surya_tablerec"
-
-    def __init__(
-        self,
-        num_hidden_layers=4,
-        vocab_size=settings.TABLE_REC_MAX_ROWS + SPECIAL_TOKENS,
-        hidden_size=1024,
-        intermediate_size=4 * 1024,
-        encoder_hidden_size=1024,
-        num_attention_heads=16,
-        lru_width=None,
-        attention_window_size=16,
-        conv1d_width=4,
-        logits_soft_cap=30.0,
-        rms_norm_eps=1e-6,
-        use_cache=True,
-        pad_token_id=0,
-        eos_token_id=1,
-        bos_token_id=2,
-        hidden_activation="gelu_pytorch_tanh",
-        rope_theta=10000.0,
-        block_types=("attention",),
-        cross_attn_layers=(0, 1, 2, 3, 4, 5),
-        self_attn_layers=(0, 1, 2, 3, 4, 5),
-        global_attn_layers=(0, 1, 2, 3, 4, 5),
-        attention_dropout=0.0,
-        num_key_value_heads=16,
-        attention_bias=False,
-        w_init_variance_scale=0.01,
-        init_std=0.02,
-        tie_word_embeddings=False,
-        causal=False,
-        max_width=BOX_DIM + SPECIAL_TOKENS,
-        max_height=BOX_DIM + SPECIAL_TOKENS,
-        max_position_embeddings=1024,
-        **kwargs,
-    ):
-        self.num_hidden_layers = num_hidden_layers
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.intermediate_size = intermediate_size
-        self.num_attention_heads = num_attention_heads
-        self.lru_width = lru_width if lru_width is not None else hidden_size
-        self.attention_window_size = attention_window_size
-        self.conv1d_width = conv1d_width
-        self.logits_soft_cap = logits_soft_cap
-        self.rms_norm_eps = rms_norm_eps
-        self.use_cache = use_cache
-        self.rope_theta = rope_theta
-        self.block_types = list(block_types)
-        self.hidden_activation = hidden_activation
-        self.head_dim = self.hidden_size // self.num_attention_heads
-        self.num_key_value_heads = num_key_value_heads if num_key_value_heads is not None else num_attention_heads
-        if self.num_key_value_heads > self.num_attention_heads:
-            raise ValueError("The number of `num_key_value_heads` must be smaller than `num_attention_heads`")
-        self.cross_attn_layers = cross_attn_layers
-        self.self_attn_layers = self_attn_layers
-        self.global_attn_layers = global_attn_layers
-        self.attention_dropout = attention_dropout
-        self.attention_bias = attention_bias
-        self.w_init_variance_scale = w_init_variance_scale
-        self.final_w_init_variance_scale = 2.0 / self.num_hidden_layers
-        self.init_std = init_std
-        self.tie_word_embeddings = tie_word_embeddings
-        self.encoder_hidden_size = encoder_hidden_size
-        self.causal = causal
-        self.max_width = max_width
-        self.max_height = max_height
-        self.max_position_embeddings = max_position_embeddings
+        self.layer_norm_eps = layer_norm_eps
+        self.dropout = dropout
+        self.bbox_size = bbox_size
+        self.pause_token_id = pause_token_id
+        self.box_properties = BOX_PROPERTIES
+        self.property_embed_size = property_embed_size
+        self.box_embed_size = box_embed_size
+        self.special_token_count = special_token_count
+        self.query_end_token_id = query_end_token_id
+        self.double_residual_flow = False
 
         super().__init__(
             pad_token_id=pad_token_id,
---
+++ surya/model/table_rec/shaper.py
@@ -0,0 +1,167 @@
+from typing import List, Dict
+import numpy as np
+import torch
+
+from surya.model.table_rec.config import BOX_PROPERTIES, SPECIAL_TOKENS, BOX_DIM
+
+
+class LabelShaper:
+    def __init__(self):
+        self.property_keys = [k for (k, kcount, mode) in BOX_PROPERTIES]
+
+    def dict_to_labels(self, label_components: List[dict]):
+        if len(label_components) == 0:
+            return []
+
+        out_list = []
+        for (k, kcount, mode) in BOX_PROPERTIES:
+            for label_component in label_components:
+                if k not in label_component:
+                    raise ValueError(f"Missing key {k} in label component {label_component}")
+
+                if mode == "classification":
+                    assert isinstance(label_component[k], int)
+                elif mode == "regression":
+                    assert (isinstance(label_component[k], (int, float)) and kcount == 1) or len(label_component[k]) == kcount
+                else:
+                    raise ValueError(f"Invalid mode {k['mode']} for key {k}")
+
+        for label_component in label_components:
+            bbox = label_component["bbox"]
+            for i in range(len(bbox)):
+                if bbox[i] < 0:
+                    bbox[i] = 0
+                if bbox[i] > BOX_DIM:
+                    bbox[i] = BOX_DIM
+
+            vector = []
+            for (k, kcount, mode) in BOX_PROPERTIES:
+                item = label_component[k]
+                if isinstance(item, (list, tuple)):
+                    vector += list(item)
+                elif isinstance(item, (float, int)):
+                    if mode == "classification":
+                        # Shift up for model
+                        item += SPECIAL_TOKENS
+                    vector.append(item)
+                else:
+                    raise ValueError(f"Invalid item {item} for key {k}")
+
+            out_list.append(vector)
+
+        return out_list
+
+    def labels_to_dict(self, labels: List[List[float]]):
+        out_list = []
+        for label in labels:
+            label_component = {}
+            start_idx = 0
+            for (k, kcount, mode) in BOX_PROPERTIES:
+                if mode == "classification":
+                    item = label[start_idx]
+                    # Shift back down for processing
+                    item -= SPECIAL_TOKENS
+                    label_component[k] = item
+                    start_idx += 1
+                elif mode == "regression":
+                    label_component[k] = label[start_idx:start_idx + kcount]
+                    start_idx += kcount
+                else:
+                    raise ValueError(f"Invalid mode {mode} for key {k}")
+
+            out_list.append(label_component)
+
+        return out_list
+
+    def component_idx(self, key):
+        idx = 0
+        for (k, kcount, mode) in BOX_PROPERTIES:
+            if mode == "regression":
+                incr = kcount
+            elif mode == "classification":
+                incr = 1
+            else:
+                raise ValueError(f"Invalid mode {mode} for key {k}")
+            if k == key:
+                return (idx, idx + incr)
+            idx += incr
+        raise ValueError(f"Key {key} not found in properties")
+
+    def get_box_property(self, key, add_special_tokens=True):
+        for (k, kcount, mode) in BOX_PROPERTIES:
+            if k == key:
+                # Add special token count
+                if mode == "classification" and add_special_tokens:
+                    kcount += SPECIAL_TOKENS
+                return (k, kcount, mode)
+        raise ValueError(f"Key {key} not found in properties")
+
+    def component_idx_dict(self):
+        idx_dict = {}
+        for (k, kcount, mode) in BOX_PROPERTIES:
+            idx_dict[k] = self.component_idx(k)
+        return idx_dict
+
+    def convert_polygons_to_bboxes(self, label_components: List[Dict]):
+        for i, label_component in enumerate(label_components):
+            poly = label_component["polygon"]
+            poly = np.clip(poly, 0, BOX_DIM)
+
+            (x1, y1), (x2, y2), (x3, y3), (x4, y4) = poly
+            cx = (x1 + x2 + x3 + x4) / 4
+            cy = (y1 + y2 + y3 + y4) / 4
+            width = (x2 + x3) / 2 - (x1 + x4) / 2
+            height = (y3 + y4) / 2 - (y2 + y1) / 2
+            bottom_avg_x = (x3 + x4) / 2
+            top_avg_x = (x1 + x2) / 2
+            right_avg_y = (y2 + y3) / 2
+            left_avg_y = (y1 + y4) / 2
+
+            x_skew = bottom_avg_x - top_avg_x
+            y_skew = right_avg_y - left_avg_y
+            x_skew += BOX_DIM // 2 # Shift up into positive space
+            y_skew += BOX_DIM // 2 # Shift up into positive space
+            new_poly = [
+                cx,
+                cy,
+                width,
+                height,
+                x_skew,
+                y_skew
+            ]
+            label_component["bbox"] = new_poly
+
+        return label_components
+
+    def convert_bbox_to_polygons(self, boxes, img_size, bbox_scaler=BOX_DIM, skew_scaler=BOX_DIM // 2, skew_min=.001):
+        w_scale = img_size[0] / bbox_scaler
+        h_scale = img_size[1] / bbox_scaler
+
+        cx = boxes[0]
+        cy = boxes[1]
+        width = boxes[2]
+        height = boxes[3]
+        x1 = cx - width / 2
+        y1 = cy - height / 2
+        x2 = cx + width / 2
+        y2 = cy + height / 2
+        skew_x = torch.floor((boxes[4] - skew_scaler) / 2)
+        skew_y = torch.floor((boxes[5] - skew_scaler) / 2)
+
+        # Ensures we don't get slightly warped boxes
+        # Note that the values are later scaled, so this is in 1/1024 space
+        skew_x[torch.abs(skew_x) < skew_min] = 0
+        skew_y[torch.abs(skew_y) < skew_min] = 0
+
+        polygon = [x1 - skew_x, y1 - skew_y, x2 - skew_x, y1 + skew_y, x2 + skew_x, y2 + skew_y, x1 + skew_x,
+                   y2 - skew_y]
+        poly = []
+        for i in range(4):
+            poly.append([
+                polygon[2 * i].item() * w_scale,
+                polygon[2 * i + 1].item() * h_scale
+            ])
+        return poly
+
+
+
