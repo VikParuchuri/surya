04db58c4196f6c30ff884d15f8c5c4f3a7daa66b
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-01-30 12:16:03 -0500

    Add mac to benchmarks

--- benchmark/table_recognition.py
+++ benchmark/table_recognition.py
@@ -132,7 +132,7 @@ def main(results_dir: str, max_rows: int, tatr: bool, debug: bool):
             "page_metrics": page_metrics
         }
 
-    with open(os.path.join(result_path, "results.json"), "w+") as f:
+    with open(os.path.join(result_path, "results.json"), "w+", encoding="utf-8") as f:
         json.dump(out_data, f, indent=4)
 
     table = [
