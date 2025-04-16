0a3351b61a1aef9d29575daa06eed1111a6bafe6
Tarun Menta <menta.tarun@gmail.com>
2025-04-15 22:19:38 -0400

    Fix graph break

--- surya/common/surya/decoder/__init__.py
+++ surya/common/surya/decoder/__init__.py
@@ -561,19 +561,19 @@ class SuryaDecoderModel(Qwen2PreTrainedModel):
         output_attentions: bool,
     ):
         if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and past_key_values is not None:
-                is_padding_right = (
-                    attention_mask[:, -1].sum().item() != input_tensor.size()[0]
-                )
-                if is_padding_right:
-                    raise ValueError(
-                        "You are attempting to perform batched generation with padding_side='right'"
-                        " this may lead to unexpected behaviour for Flash Attention version of Qwen2. Make sure to "
-                        " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
-                    )
-            if attention_mask is not None and 0.0 in attention_mask:
-                return attention_mask
-            return None
+            # if attention_mask is not None and past_key_values is not None:
+            #     is_padding_right = (
+            #         attention_mask[:, -1].sum().item() != input_tensor.size()[0]
+            #     )
+            #     if is_padding_right:
+            #         raise ValueError(
+            #             "You are attempting to perform batched generation with padding_side='right'"
+            #             " this may lead to unexpected behaviour for Flash Attention version of Qwen2. Make sure to "
+            #             " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
+            #         )
+            # if attention_mask is not None and 0.0 in attention_mask:
+            #     return attention_mask
+            return attention_mask
 
         # For SDPA, when possible, we will rely on its `is_causal` argument instead of its `attn_mask` argument, in
         # order to dispatch on Flash Attention 2. This feature is not compatible with static cache, as SDPA will fail
