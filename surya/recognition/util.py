19245ff601ae6886e5034ac0812907c6750e9700
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-05-28 20:52:28 -0400

    Remove parens

--- surya/recognition/util.py
+++ surya/recognition/util.py
@@ -7,7 +7,7 @@ import torch
 from surya.common.polygon import PolygonBox
 from surya.recognition.schema import TextLine, TextWord, TextChar
 
-MATH_SYMBOLS = ["+", "-", "*", "/", "=", "^", "_", "\\", "(", ")", "{", "}"]
+MATH_SYMBOLS = ["+", "-", "*", "/", "=", "^", "_", "\\", "{", "}"]
 
 
 def unwrap_math(text: str) -> str:
